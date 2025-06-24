590deda1b68d34cff1f2d59bcf00058fabfad117
saathvikc <83232359+saathvikc@users.noreply.github.com>
2025-06-24 17:27:44 -0400

    began data processing

---
+++ data_processing.py
@@ -0,0 +1,12 @@
+import os
+import lzma
+from tqdm import tqdm
+
+def xz_files_in_dir(directory):
+    files = []
+    for filename in os.listdir(directory):
+        if filename.endswith('.xz') and os.path.isfile(os.path.join(directory, filename)):
+            files.append(filename)
+    return files
+
+ 
